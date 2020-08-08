b6c2ad3d370428c8ad3e062c060b5aea459bd319
Dogun Oghenekobiruo <34072087+Dogunfx@users.noreply.github.com>
2020-08-07 17:36:42 -0700

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,25 @@
-import Link from 'next/link'
+import { useEffect } from "react";
+import Link from "next/link";
+import io from "socket.io-client";
 
 export default function Home() {
+  useEffect(() => {
+    const client = io();
+  });
   return (
-    <ul>
-      <li>
-        <Link href="/b" as="/a">
-          <a>a</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/a" as="/b">
-          <a>b</a>
-        </Link>
-      </li>
-    </ul>
-  )
+    <>
+      <ul>
+        <li>
+          <Link href="/b" as="/a">
+            <a>a</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/a" as="/b">
+            <a>b</a>
+          </Link>
+        </li>
+      </ul>
+    </>
+  );
 }
--- server.js
+++ server.js
@@ -1,28 +1,38 @@
-const express = require('express')
-const next = require('next')
+const express = require("express");
+const next = require("next");
 
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+
+// Socket implementation starts here
+
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  const server = express()
+  const server = express();
 
-  server.get('/a', (req, res) => {
-    return app.render(req, res, '/a', req.query)
-  })
+  var http = require("http").createServer(server);
+  var io = require("socket.io")(http);
 
-  server.get('/b', (req, res) => {
-    return app.render(req, res, '/b', req.query)
-  })
+  server.get("/a", (req, res) => {
+    return app.render(req, res, "/a", req.query);
+  });
 
-  server.all('*', (req, res) => {
-    return handle(req, res)
-  })
+  server.get("/b", (req, res) => {
+    return app.render(req, res, "/b", req.query);
+  });
 
-  server.listen(port, (err) => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
-  })
-})
+  server.all("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  io.on("connection", socket => {
+    console.log("a user connected");
+  });
+
+  http.listen(port, err => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
